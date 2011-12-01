c1206cb0b00a741a3743eff83cae127ec917ffc8
liu <liu@localhost.(none)>
2011-12-01 09:06:37 +0800

    Initial Commit

---
+++ ShelfReader/ShelfReaderAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ShelfReaderAppDelegate.h
+//  ShelfReader
+//
+//  Created by liu liu on 11-12-1.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShelfReaderAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShelfReader/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  ShelfReader
+//
+//  Created by liu liu on 11-12-1.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ShelfReaderAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([ShelfReaderAppDelegate class]));
+    }
+    return retVal;
+}
